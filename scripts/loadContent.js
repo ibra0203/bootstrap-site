00c7087a1ecc319868444ac547df67a49e70443a
mody1710 <ibra0203@algonquinlive.com>
2018-01-10 02:10:30 -0400

    build4

--- scripts/loadContent.js
+++ scripts/loadContent.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     /*$.get("sub-content/loaded-header.html",function(_c){
         $(body).prepend(_c);
     });*/
+    $("#test2").load("loaded-header.html #test1");
     $(".test").load("../scripts/loaded-header.html #header1");
 
 });
